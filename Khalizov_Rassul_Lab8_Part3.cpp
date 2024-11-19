6bde8b4da5c77e4c886892500e286b3f223ef35e
Rassul-creator-commits <rassulk@nycstudents.net>
2024-11-18 21:01:25 -0500

    Update Khalizov_Rassul_Lab8_Part3.cpp

--- Khalizov_Rassul_Lab8_Part3.cpp
+++ Khalizov_Rassul_Lab8_Part3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 double getSum(double num1, double num2, double num3) {
@@ -16,7 +17,7 @@ int main() {
     double result = getSum(a, b, c);
     
     // Outputs the result
-    cout << "The sum of the three numbers is: " << result << std::endl;
+    cout << "The sum of the three numbers is: " << result << endl;
 
     return 0;
     /*
