aeb9c1d93f8d2ce7b341ff24352a77b50ec9628b
Rassul-creator-commits <rassulk@nycstudents.net>
2024-11-18 20:55:22 -0500

    Update Khalizov_Rassul_Lab8_Part1.cpp

--- Khalizov_Rassul_Lab8_Part1.cpp
+++ Khalizov_Rassul_Lab8_Part1.cpp
@@ -1,1 +1,25 @@
+include <iostream>   // enables inputs and outputs operations
+#include <cmath>      // enables sqt, power, and fllor
 
+using namespace std; 
+
+int main() {
+    double num;
+    
+    // asks the user to input a float point number
+    cout << "Enter a floating point number: ";
+    cin >> num;
+    
+    // calculates what the square root, fourt power, and floor of the number is and outputs it
+    cout << "Square root: " << sqrt(num) << std::endl;
+    cout << "Fourth power: " << pow(num, 4) << std::endl;
+    cout << "Floor: " << floor(num) << std::endl;
+    
+    return 0;
+    /*
+    Enter a floating point number: 1.1111
+    Square root: 1.05409
+    Fourth power: 1.5241
+    Floor: 1
+    */
+}
